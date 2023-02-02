24cdcdb23d08ce30f13099b12d9a0e7e5ada5ab8
mo <nataleshen.ko@yandex.ru>
2023-02-02 17:11:39 +0300

    initial-commit

---
+++ src/main/java/com/malinaink/junittestuser/JUnitTestUserApplication.java
@@ -0,0 +1,13 @@
+package com.malinaink.junittestuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitTestUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malinaink/junittestuser/JUnitTestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malinaink.junittestuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
